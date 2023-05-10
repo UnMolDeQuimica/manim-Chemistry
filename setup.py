56b9b27ca6daf28c095ed36bc736a5fa13e4e193
Un Mol De Quimica <unmoldequimica@gmail.com>
2023-05-10 18:54:18 +0200

    Added entry point

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup()
+    setup(
+        entry_points={
+            'manim.plugins': [
+                'manim_chemistry = src.manim_chemistry',
+            ],
+        },
+    )
+
