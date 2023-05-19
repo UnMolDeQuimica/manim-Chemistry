7f1f70a4c909a01b505ea470a0901f55a350f8d7
Un Mol De Quimica <unmoldequimica@gmail.com>
2023-05-19 18:28:59 +0200

    Yoinks the version setup and moves stuff to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,11 +1,5 @@
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup(
-        entry_points={
-            'manim.plugins': [
-                'manim_chemistry = manim_chemistry',
-            ],
-        },
-    )
+    setup()
 
--- src/manim_chemistry/__init__.py
+++ src/manim_chemistry/__init__.py
@@ -6,3 +6,10 @@ from .orbitals import *
 from .bohr_atom import *
 from .utils import *
 
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = "unknown"
