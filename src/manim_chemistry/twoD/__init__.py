90c857361f37a80e9490db85474f7ed64ce309e7
Miguel Ángel Rodríguez Vidal <unmoldequimica@gmail.com>
2023-12-08 21:43:40 +0100

    Added chemical formulas

--- src/manim_chemistry/twoD/__init__.py
+++ src/manim_chemistry/twoD/__init__.py
@@ -1,3 +1,4 @@
 from .atom import MAtomObject
 from .bond import BaseMBondObject, SimpleBond, DoubleBond, TripleBond
 from .molecule import MMoleculeObject, NamedMolecule
+from .chemical_formula import ChemicalFormula, NamedFormula, ComplexFormula, NamedComplexFormula
