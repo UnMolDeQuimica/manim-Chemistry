41635c450ec463f65bb5edcd723e4b7a4369da06
UnMolDeQuimica <unmoldequimica@gmail.com>
2024-10-13 19:02:25 +0200

    feat: Modified sphinx config

---
+++ docs/source/conf.py
@@ -0,0 +1,40 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+
+
+project = 'manim-Chemistry'
+copyright = '2024, Miguel Ángel Rodríguez Vidal'
+author = 'Miguel Ángel Rodríguez Vidal'
+release = '0.4.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.todo", 
+    "sphinx.ext.viewcode", 
+    "sphinx.ext.autodoc", 
+    "myst_parser",
+    "sphinx_copybutton"
+    ]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
