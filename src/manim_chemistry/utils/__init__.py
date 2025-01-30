0e0af8393f9de2c0ced42146ee66a1a3cf0b1e61
Miguel Ángel Rodríguez Vidal <marodriguez@apsl.net>
2025-01-30 17:11:17 +0100

    Formatted file with ruff

--- src/manim_chemistry/utils/__init__.py
+++ src/manim_chemistry/utils/__init__.py
@@ -1,2 +1,7 @@
-from .utils import (mol_parser, mol_parser_string, mol_to_graph,  # noqa F841
-                    sdf_parser, sdf_parser_string)
+from .utils import (
+    mol_parser,
+    mol_parser_string,
+    mol_to_graph,  # noqa F841
+    sdf_parser,
+    sdf_parser_string,
+)
