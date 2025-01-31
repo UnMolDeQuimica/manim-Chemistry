1438f61c3892b1b2dc1e6170f3fd0a1054f1aa3c
Miguel Ángel Rodríguez Vidal <unmoldequimica@gmail.com>
2025-01-31 23:09:22 +0100

    feat: Added import parsers to utils

--- src/manim_chemistry/utils/__init__.py
+++ src/manim_chemistry/utils/__init__.py
@@ -1,7 +1,12 @@
 from .utils import (
-    mol_parser,
-    mol_parser_string,
+    mol_parser,  # noqa F841
+    mol_parser_string,  # noqa F841
     mol_to_graph,  # noqa F841
-    sdf_parser,
-    sdf_parser_string,
+    sdf_parser,  # noqa F841
+    sdf_parser_string,  # noqa F841
+)
+
+from .parsers import (
+    MolParser,  # noqa F841
+    SDFParser,   # noqa F841
 )
