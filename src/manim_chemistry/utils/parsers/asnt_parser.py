890212e420b9c62eb1f1c3b70f05b593d2854399
Miguel Ángel Rodríguez Vidal <unmoldequimica@gmail.com>
2025-02-06 22:24:27 +0100

    Formatted with Ruff

--- src/manim_chemistry/utils/parsers/asnt_parser.py
+++ src/manim_chemistry/utils/parsers/asnt_parser.py
@@ -11,6 +11,7 @@ BOND_TYPE_MAPPING = {
     "triple": 3,
 }
 
+
 class ASNTParser(BaseParser):
     """
     The ASNT format is weird. It's like a JSON wannabe but ugly.
@@ -29,8 +30,8 @@ class ASNTParser(BaseParser):
     @staticmethod
     def replace_stuff(line: str):
         line = line.strip().replace("\n", "")
-        line = line.replace('"', '¿')
-        line = line.replace("'", '¡')
+        line = line.replace('"', "¿")
+        line = line.replace("'", "¡")
         if line.startswith("{"):
             if len(line) == 1:
                 # That means the line is {
@@ -82,43 +83,45 @@ class ASNTParser(BaseParser):
             line = ASNTParser.replace_stuff(lines_list.pop(0))
 
         atoms_indices = []
-        lines_list.pop(0) # Remove atoms line
-        line = ASNTParser.replace_stuff(lines_list.pop(0)) # Start atoms
+        lines_list.pop(0)  # Remove atoms line
+        line = ASNTParser.replace_stuff(lines_list.pop(0))  # Start atoms
         while not line.startswith("}"):
             atoms_indices.append(line.replace(",", "").replace('"', ""))
             line = ASNTParser.replace_stuff(lines_list.pop(0))
 
         atoms_indices = atoms_indices
         atoms_elements = []
-        lines_list.pop(0) # Remove element line
-        line = ASNTParser.replace_stuff(lines_list.pop(0)) # Get first element
+        lines_list.pop(0)  # Remove element line
+        line = ASNTParser.replace_stuff(lines_list.pop(0))  # Get first element
         while not line.startswith("}"):
             atoms_elements.append(line.replace(",", "").replace('"', "").capitalize())
             line = ASNTParser.replace_stuff(lines_list.pop(0))
 
-        atoms_data = {int(float(atom_index.strip())): {"element": atom_element} for atom_index, atom_element in zip(atoms_indices, atoms_elements)}
-
+        atoms_data = {
+            int(float(atom_index.strip())): {"element": atom_element}
+            for atom_index, atom_element in zip(atoms_indices, atoms_elements)
+        }
 
         from_atom_index = []
 
-        lines_list.pop(0) # Remove }, line
-        lines_list.pop(0) # Remove bonds line
-        lines_list.pop(0) # Remove aid1 line
-        line = ASNTParser.replace_stuff(lines_list.pop(0)) # Get first from atom index
+        lines_list.pop(0)  # Remove }, line
+        lines_list.pop(0)  # Remove bonds line
+        lines_list.pop(0)  # Remove aid1 line
+        line = ASNTParser.replace_stuff(lines_list.pop(0))  # Get first from atom index
         while not line.startswith("}"):
             from_atom_index.append(int(line.replace(",", "").replace('"', "")))
             line = ASNTParser.replace_stuff(lines_list.pop(0))
 
         to_atom_index = []
-        lines_list.pop(0) # Remove aid2 line
-        line = ASNTParser.replace_stuff(lines_list.pop(0)) # Get first to atom index
+        lines_list.pop(0)  # Remove aid2 line
+        line = ASNTParser.replace_stuff(lines_list.pop(0))  # Get first to atom index
         while not line.startswith("}"):
             to_atom_index.append(int(line.replace(",", "").replace('"', "")))
             line = ASNTParser.replace_stuff(lines_list.pop(0))
 
         bond_type_list = []
-        lines_list.pop(0) # Remove aid2 line
-        line = ASNTParser.replace_stuff(lines_list.pop(0)) # Get first bond
+        lines_list.pop(0)  # Remove aid2 line
+        line = ASNTParser.replace_stuff(lines_list.pop(0))  # Get first bond
         while not line.startswith("}"):
             bond_type_list.append(line.replace(",", ""))
             line = ASNTParser.replace_stuff(lines_list.pop(0))
@@ -127,33 +130,48 @@ class ASNTParser(BaseParser):
             line = ASNTParser.replace_stuff(lines_list.pop(0))
 
         bonds_data = {}
-        for bond_index, bond_params in enumerate(zip(from_atom_index, to_atom_index, bond_type_list)):
+        for bond_index, bond_params in enumerate(
+            zip(from_atom_index, to_atom_index, bond_type_list)
+        ):
             from_atom, to_atom, bond_type = bond_params
             bonds_data[bond_index] = {
                 "from_atom_index": from_atom,
                 "to_atom_index": to_atom,
-                "bond_type": BOND_TYPE_MAPPING.get(bond_type.replace('"', ""), 1)
-                }
+                "bond_type": BOND_TYPE_MAPPING.get(bond_type.replace('"', ""), 1),
+            }
 
         coords_x = {}
-        lines_list.pop(0) # Remove { line
-        lines_list.pop(0) # Remove x { line
+        lines_list.pop(0)  # Remove { line
+        lines_list.pop(0)  # Remove x { line
         line = ASNTParser.replace_stuff(lines_list.pop(0))
         for i in atoms_data.keys():
-            line = line.replace("[", "").replace("]", "").replace(",", "").strip().split(" ")
+            line = (
+                line.replace("[", "")
+                .replace("]", "")
+                .replace(",", "")
+                .strip()
+                .split(" ")
+            )
             coords_x[i] = int(line[0]) * int(line[1]) ** int(line[2])
             line = ASNTParser.replace_stuff(lines_list.pop(0))
 
         coords_y = {}
-        lines_list.pop(0) # Remove y { line
+        lines_list.pop(0)  # Remove y { line
         line = ASNTParser.replace_stuff(lines_list.pop(0))
         for i in atoms_data.keys():
-            line = line.replace("[", "").replace("]", "").replace(",", "").strip().split(" ")
+            line = (
+                line.replace("[", "")
+                .replace("]", "")
+                .replace(",", "")
+                .strip()
+                .split(" ")
+            )
             coords_y[i] = int(line[0]) * int(line[1]) ** int(line[2])
             line = ASNTParser.replace_stuff(lines_list.pop(0))
 
         for index in atoms_data.keys():
-            atoms_data[index]["coords"] = np.array([coords_x[index], coords_y[index], 0])
+            atoms_data[index]["coords"] = np.array(
+                [coords_x[index], coords_y[index], 0]
+            )
 
         return atoms_data, bonds_data
-
