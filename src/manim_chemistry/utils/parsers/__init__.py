a6d693f68614ad83d5c828ae3d6b9b8a7892905b
Miguel Ángel Rodríguez Vidal <unmoldequimica@gmail.com>
2025-02-07 11:58:19 +0100

    feat: Added JSONParser

--- src/manim_chemistry/utils/parsers/__init__.py
+++ src/manim_chemistry/utils/parsers/__init__.py
@@ -1,3 +1,4 @@
 from .mol_parser import MolParser  # noqa F841
 from .sdf_parser import SDFParser  # noqa F841
 from .asnt_parser import ASNTParser  # noqa F841
+from .json_parser import JSONParser  # noqa F841
