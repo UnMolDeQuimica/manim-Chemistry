3974035567df65865bf19cd288372a44997d02d9
Miguel Ángel Rodríguez Vidal <unmoldequimica@gmail.com>
2025-02-17 00:06:03 +0100

    feat: Added multiple entries to options and shorthand versions

--- src/manim_chemistry/cli.py
+++ src/manim_chemistry/cli.py
@@ -12,18 +12,40 @@ def cli(): ...
 @click.command()
 @click.option(
     "--format",
+    "-f",
     default="sdf",
     help="Format of the file to be downloaded. Defaults to 'sdf'. Options are 'asnt', 'json', 'sdf' and 'xlm'.",
 )
-@click.option("--cid", default=None, help="cid of the molecule.")
-@click.option("--name", default=None, help="name of the molecule.")
-@click.option("--smiles", default=None, help="smiles of the molecule.")
-@click.option("--inchi", default=None, help="inchi of the molecule.")
-@click.option("--three_d", default=False, help="Use three d data of the molecule.")
-@click.option("--output_folder", default=".", help="Output folder.")
+@click.option("--cid", "-c", multiple=True, help="cid of the molecule.")
+@click.option("--name", "-n", multiple=True, help="name of the molecule.")
+@click.option("--smiles", "-s", multiple=True, help="smiles of the molecule.")
+@click.option("--inchi", "-i", multiple=True, help="inchi of the molecule.")
+@click.option(
+    "--three_d", "-td", default=False, help="Use three d data of the molecule."
+)
+@click.option("--output_folder", "-o", default=".", help="Output folder.")
 def pubchem_molecule(format, cid, name, smiles, inchi, three_d, output_folder):
     """
     Download molecule from pubchem.
+
+    Supports downloading multiple files from a single request.
+
+    Example
+    -------
+
+    .. code-block:: console
+
+        manim_chemistry pubchem-molecule --format sdf -n acetone -n morphine
+
+    Result:
+
+    .. code-block:: console
+
+        >>> Retrieving molecule data for ('acetone', 'morphine').
+        >>> Retrieved molecule data for ('acetone', 'morphine'). Saving file(s) to . folder.
+        >>> File .\\acetone.sdf is ready!!
+        >>> File .\\morphine.sdf is ready!!
+        >>> Finished
     """
 
     identifier = cid or name or smiles or inchi
@@ -34,36 +56,52 @@ def pubchem_molecule(format, cid, name, smiles, inchi, three_d, output_folder):
             "No identifier provided. Options are 'cid', 'name', 'smile' and 'inchi'."
         )
 
+    molecules = {}
     if cid:
-        molecule_data = PubchemAPIManager(
-            cid=cid, three_d=three_d, format=format
-        ).get_molecule()
+        for molecule in cid:
+            molecule_data = PubchemAPIManager(
+                cid=molecule, three_d=three_d, format=format
+            ).get_molecule()
+
+            molecules[molecule] = molecule_data
 
     elif name:
-        molecule_data = PubchemAPIManager(
-            name=name, three_d=three_d, format=format
-        ).get_molecule()
+        for molecule in name:
+            molecule_data = PubchemAPIManager(
+                name=molecule, three_d=three_d, format=format
+            ).get_molecule()
+
+            molecules[molecule] = molecule_data
 
     elif smiles:
-        molecule_data = PubchemAPIManager(
-            smiles=smiles, three_d=three_d, format=format
-        ).get_molecule()
+        for molecule in smiles:
+            molecule_data = PubchemAPIManager(
+                smiles=molecule, three_d=three_d, format=format
+            ).get_molecule()
+
+            molecules[molecule] = molecule_data
 
     else:
         # InChI is used
-        molecule_data = PubchemAPIManager(
-            inchi=inchi, three_d=three_d, format=format
-        ).get_molecule()
+        for molecule in inchi:
+            molecule_data = PubchemAPIManager(
+                inchi=molecule, three_d=three_d, format=format
+            ).get_molecule()
 
-    output_file = os.path.join(output_folder, f"{identifier}.{format}")
+            molecules[molecule] = molecule_data
+
     click.echo(
-        f"Retrieved molecule data for {identifier}. Saving file to {output_file}."
+        f"Retrieved molecule data for {identifier}. Saving file(s) to {output_folder} folder."
     )
 
-    with open(output_file, "w") as file:
-        file.write(molecule_data)
+    for molecule_identifier, molecule_data in molecules.items():
+        output_file = os.path.join(output_folder, f"{molecule_identifier}.{format}")
+        with open(output_file, "w") as file:
+            file.write(molecule_data)
 
-    click.echo(f"File {output_file} is ready!!")
+        click.echo(f"File {output_file} is ready!!")
+
+    click.echo("Finished")
 
 
 cli.add_command(pubchem_molecule)
